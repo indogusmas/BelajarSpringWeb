adacd5e20ccb67879da94e74233084935df8f98c
ACER <https://github.com/indogusmas/UjicobaUpload.git>
2018-11-20 18:56:17 +0700

    add model mahasiswa

---
+++ src/main/java/com/indogusmas/BelajarSpringWeb/model/Mahasiswa.java
@@ -0,0 +1,49 @@
+package com.indogusmas.BelajarSpringWeb.model;
+
+import com.sun.javafx.beans.IDProperty;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="tblMahasisa")
+public class Mahasiswa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  Integer id;
+    private  String nim;
+    private  String name;
+    private  String jurusan;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+}
